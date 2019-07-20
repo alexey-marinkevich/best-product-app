ac5de53fcc72c00aedc23cfd9f7341cc79138474
Alex Marinkevich <bitrate595@gmail.com>
2019-07-20 13:30:23 +0300

    Add base route, create and stylize produrct page

---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './Header';
+import ProductItems from './ProductItems';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header/>
+      <ProductItems/>
+    </div>
+  )
+}
+
+export default HomePage;
