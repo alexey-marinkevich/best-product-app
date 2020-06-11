270a8adfae4d5c45210ea04d1fb70aa6cdbae40f
Alex Marinkevich <bitrate595@gmail.com>
2020-06-11 12:03:53 +0300

    Change packages configuration

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as PreviewProductPage } from './PreviewProductPage';
-export { default as ProductDetailsPage } from './ProductDetailsPage/ProductDetailsPage';
-export { default as SuggestProductPage } from './SuggestProductPage/SuggestProductPage';
