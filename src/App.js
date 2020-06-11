c88f6762df0d65f691ec5a1f61a54942eaf4bb3a
Alex Marinkevich <bitrate595@gmail.com>
2020-06-11 12:09:36 +0300

    Add code splitting, eslint, lint staged fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import {
-  HomePage,
-  ProductDetailsPage,
-  SuggestProductPage,
-  PreviewProductPage,
-} from './pages';
+import Loader from './components/Loader';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const SuggestProductPage = lazy(() => import('./pages/SuggestProductPage/SuggestProductPage'));
+const ProductDetailsPage = lazy(() => import('./pages/ProductDetailsPage/ProductDetailsPage'));
+const PreviewProductPage = lazy(() => import('./pages/PreviewProductPage'));
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/product/:id" component={ProductDetailsPage} />
-        <Route exact path="/suggest-form" component={SuggestProductPage} />
-        <Route path="/suggest-form/product-preview" component={PreviewProductPage} />
-      </Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/product/:id" component={ProductDetailsPage} />
+          <Route exact path="/suggest-form" component={SuggestProductPage} />
+          <Route path="/suggest-form/product-preview" component={PreviewProductPage} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
